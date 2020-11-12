1d1ce0e0c46829c8e836ff123adbaded22fe5a56
Ben Zelinski <bziggz@gmail.com>
2020-11-12 17:31:38 -0500

    Better prompt package (Prompts). Setup Functionality Complete.

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,28 +1,8 @@
 const fs = require("fs");
-const prompt = require("prompt");
+const prompt = require("prompts");
 prompt.message = "🔆 ";
 prompt.delimiter = "";
 prompt.colors = false;
-
-const writeToFile = (api, email) => {
-  fs.writeFileSync("../../.env", `APIKEY=${api}\nEMAIL=${email}`);
-};
-
-const retrieveExistingValues = () => {
-  const existingENV = fs.readFileSync("../../.env", "utf8");
-  console.log(existingENV);
-};
-
-const promptUser = () => {
-  const emailMsg = "Enter the email associated with your Cloudflare account:";
-  const apiMsg = "Enter your Cloudflare Global API Key:";
-
-  prompt.start();
-  prompt.get([emailMsg, apiMsg], (err, result) => {
-    writeToFile(result[apiMsg], result[emailMsg]);
-    configGoodbye();
-  });
-};
 
 const configMsg = () => {
   console.log("Campion Config\n");
@@ -32,13 +12,48 @@ const configGoodbye = () => {
   console.log("Setup complete.");
 };
 
-const setup = () => {
+const retrieveExistingValues = () => {
+  const existingENV = fs.readFileSync("../../.env", "utf8").split("\n");
+
+  const apiKey = existingENV[0].slice(7);
+  const email = existingENV[1].slice(6);
+
+  return [apiKey, email];
+};
+
+const promptUser = async (apiKey, email) =>
+  await prompt(questions(apiKey, email));
+
+const writeToFile = ({ apiKey, email }) => {
+  fs.writeFileSync("../../.env", `APIKEY=${apiKey}\nEMAIL=${email}`);
+};
+
+const questions = (apiKey, email) => [
+  {
+    type: "text",
+    name: "email",
+    message: "Enter the email associated with your Cloudflare account:",
+    initial: email || "",
+  },
+  {
+    type: "text",
+    name: "apiKey",
+    message: "Enter your Cloudflare Global API Key:",
+    initial: apiKey || "",
+  },
+];
+
+const setup = async () => {
   configMsg();
+
   try {
-    retrieveExistingValues();
+    const [apiKey, email] = retrieveExistingValues();
+    writeToFile(await promptUser(apiKey, email));
   } catch (e) {
-    promptUser();
+    writeToFile(await promptUser());
   }
+
+  configGoodbye();
 };
 
 module.exports = setup;
