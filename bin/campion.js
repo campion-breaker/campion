6d22c3bdc11b08a5243624de0a9b24e8f3c82997
Arthur Kauffman <arthur@sent.com>
2020-11-12 15:19:50 -0500

    Basic file structure

--- bin/campion.js
+++ bin/campion.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+const minimist = require("minimist");
+const argv = minimist(process.argv.slice(2), {});
+
+switch (argv._[0]) {
+  case "setup":
+    require("../src/commands/setup")();
+    break;
+  case "build":
+    require("../src/commands/build")();
+    break;
+  default:
+    console.log("help");
+    break;
+}
