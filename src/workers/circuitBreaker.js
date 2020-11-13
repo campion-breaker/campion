1f4d2f10d1628aa0573dca12830331b0e634ee5c
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-13 11:46:40 -0500

    Added functionality to check circuit state and return if state is in open or half-open mode.

--- src/workers/circuitBreaker.js
+++ src/workers/circuitBreaker.js
@@ -2,6 +2,14 @@ async function handleRequest(request) {
   const serviceId = getServiceId(request.url);
   const service = getResourceObj(serviceId);
   if (service === null) return new Response("Circuit breaker doesn't exist", {status: 404});
+
+  const state = circuitState(service);
+
+  if (state === "OPEN") {
+    return new Response('Circuit is open', {status: 504});
+  } else if (state === "HALF-OPEN") {
+    return new Response('Circuit is closed', {status: 504});
+  }
 }
 
 function getServiceId(url) {
@@ -11,4 +19,8 @@ function getServiceId(url) {
 async function getResourceObj(serviceId) {
   const service = await RESOURCES.get(serviceId);
   return JSON.parse(service);
+}
+
+function circuitState(service) {
+  return service.STATE;
 }
