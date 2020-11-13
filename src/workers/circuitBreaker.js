2db7b28ab9ed266dcfba2a89309d9d1dfec59a12
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-13 11:40:56 -0500

    Extracted url parsing to it's own function and added functionality to get the resource obj from KV.

--- src/workers/circuitBreaker.js
+++ src/workers/circuitBreaker.js
@@ -1,5 +1,14 @@
 async function handleRequest(request) {
-  const serviceId = new URL(request.url).pathname.replace('/', '');
-  const service = await RESOURCES.get(serviceId);
+  const serviceId = getServiceId(request.url);
+  const service = getResourceObj(serviceId);
   if (service === null) return new Response("Circuit breaker doesn't exist", {status: 404});
+}
+
+function getServiceId(url) {
+  return new URL(request.url).pathname.replace('/', '');
+}
+
+async function getResourceObj(serviceId) {
+  const service = await RESOURCES.get(serviceId);
+  return JSON.parse(service);
 }
