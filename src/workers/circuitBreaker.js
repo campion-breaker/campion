730ef90ba28dfeda1a38baad1399c30ef551db76
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-13 11:49:13 -0500

    Added canRequestProceed function to allow some half-open requests to proceed.

--- src/workers/circuitBreaker.js
+++ src/workers/circuitBreaker.js
@@ -1,13 +1,13 @@
 async function handleRequest(request) {
   const serviceId = getServiceId(request.url);
-  const service = getResourceObj(serviceId);
+  const service = getServiceObj(serviceId);
   if (service === null) return new Response("Circuit breaker doesn't exist", {status: 404});
 
   const state = circuitState(service);
 
   if (state === "OPEN") {
     return new Response('Circuit is open', {status: 504});
-  } else if (state === "HALF-OPEN") {
+  } else if (state === "HALF-OPEN" && !canRequestProceed(serviceObj)) {
     return new Response('Circuit is closed', {status: 504});
   }
 }
@@ -16,11 +16,18 @@ function getServiceId(url) {
   return new URL(request.url).pathname.replace('/', '');
 }
 
-async function getResourceObj(serviceId) {
+async function getServiceObj(serviceId) {
   const service = await RESOURCES.get(serviceId);
   return JSON.parse(service);
 }
 
 function circuitState(service) {
   return service.STATE;
+}
+
+function canRequestProceed(service) {
+  const min = 1;
+  const max = Math.floor(100 / service.PERCENT_OF_REQUESTS);
+  const randNum = Math.floor(Math.random() * (max - min + 1) + min);
+  return randNum === 1;
 }
