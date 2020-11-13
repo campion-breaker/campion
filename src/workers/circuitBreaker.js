54ffd2149f814aef334437a9d058209698cc6630
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-13 11:58:23 -0500

    Added the timeoutPromise into the processRequest function to handle scenarios when the request takes too long.

--- src/workers/circuitBreaker.js
+++ src/workers/circuitBreaker.js
@@ -1,3 +1,5 @@
+const { v4: UUID } = require('uuid');
+
 async function handleRequest(request) {
   const serviceId = getServiceId(request.url);
   const service = getServiceObj(serviceId);
@@ -10,6 +12,8 @@ async function handleRequest(request) {
   } else if (state === "HALF-OPEN" && !canRequestProceed(serviceObj)) {
     return new Response('Circuit is closed', {status: 504});
   }
+
+  const response = await processRequest(request, service);
 }
 
 function getServiceId(url) {
@@ -30,4 +34,14 @@ function canRequestProceed(service) {
   const max = Math.floor(100 / service.PERCENT_OF_REQUESTS);
   const randNum = Math.floor(Math.random() * (max - min + 1) + min);
   return randNum === 1;
+}
+
+async function processRequest(request, serviceObj) {
+  let timeoutId;
+
+  const timeoutPromise = new Promise((resolutionFunc, rejectionFunc) => {
+    timeoutId = setTimeout(() => {
+      resolutionFunc({ failure: true, kvKey: "@NETWORK_FAILURE_" + UUID(), status: 522 });
+    }, serviceObj.MAX_LATENCY);
+  });
 }
