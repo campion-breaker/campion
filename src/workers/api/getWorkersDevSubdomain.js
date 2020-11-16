6718e80b4ce012e5477b0b8f61d4a0e5a7f55c13
Arthur Kauffman <arthur@sent.com>
2020-11-16 18:39:48 -0500

    Add get workers.dev subdomain functionality

---
+++ src/workers/api/getWorkersDevSubdomain.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+const configDir = require("../../utils/configDir");
+require("dotenv").config({ path: `${configDir}/.env` });
+
+const getWorkersDevSubdomain = async () => {
+  const acctId = process.env.ACCOUNT_ID;
+
+  const data = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${acctId}/workers/subdomain`,
+    {
+      headers: {
+        "X-Auth-Email": process.env.EMAIL,
+        "X-Auth-Key": process.env.APIKEY,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!data.ok) {
+    throw new Error("\nGET worker subdomain failed.\n" + data.statusText);
+  }
+
+  const body = await data.json();
+  return `https://campion.${body.result.subdomain}.workers.dev/`;
+};
+
+module.exports = getWorkersDevSubdomain;
