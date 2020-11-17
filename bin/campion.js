dc19cd9104f2e67b8d60bad3532f4761731e5ba4
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-17 15:39:03 -0500

    Added update command and required it.

--- bin/campion.js
+++ bin/campion.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
-const minimist = require("minimist");
+const minimist = require('minimist');
 const argv = minimist(process.argv.slice(2), {});
 
 switch (argv._[0]) {
-  case "setup":
-    require("../src/commands/setup")();
+  case 'setup':
+    require('../src/commands/setup')();
     break;
-  case "new":
-  case "add":
-    require("../src/commands/add")();
+  case 'new':
+  case 'add':
+    require('../src/commands/add')();
     break;
-  case "list":
-    require("../src/commands/list")();
+  case 'list':
+    require('../src/commands/list')();
+    break;
+  case 'update':
+    require('../src/commands/update')();
     break;
   default:
-    console.log("help");
+    console.log('help');
     break;
 }
