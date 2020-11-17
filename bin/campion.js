87dd02435bf5bdacc5abbc87204303c5a318971b
Ben Zelinski <bziggz@gmail.com>
2020-11-17 17:04:32 -0500

    Delete command added to campion.js

--- bin/campion.js
+++ bin/campion.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
 
-const minimist = require('minimist');
+const minimist = require("minimist");
 const argv = minimist(process.argv.slice(2), {});
 
-switch (argv._[0]) {
-  case 'setup':
-    require('../src/commands/setup')();
+switch (argv._[0].toLowerCase()) {
+  case "setup":
+    require("../src/commands/setup")();
     break;
-  case 'new':
-  case 'add':
-    require('../src/commands/add')();
+  case "new":
+  case "add":
+    require("../src/commands/add")();
     break;
-  case 'list':
-    require('../src/commands/list')();
+  case "list":
+    require("../src/commands/list")();
     break;
-  case 'update':
-    require('../src/commands/update')();
+  case "update":
+    require("../src/commands/update")();
+    break;
+  case "delete":
+    require("../src/commands/delete")();
     break;
   default:
-    console.log('help');
+    console.log("help");
     break;
 }
