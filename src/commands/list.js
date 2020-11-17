317453345f9415519ebcb6968105db0362ed315d
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-17 14:03:18 -0500

    Displayed content in table format.

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,5 +1,12 @@
-const getServiceConfig = require("../workers/api/getServiceConfig");
-const getAllServices = require("../workers/api/getAllServices");
+const getServiceConfig = require('../workers/api/getServiceConfig');
+const getAllServices = require('../workers/api/getAllServices');
+const configDir = require('../utils/configDir');
+require('dotenv').config({ path: `${configDir}/.env` });
+const Table = require('cli-table3');
+
+const table = new Table({
+  head: ['Name', 'Circuit State', 'Campion Endpoint'],
+});
 
 const list = async () => {
   const services = await getAllServices();
@@ -7,10 +14,14 @@ const list = async () => {
 
   for (let i = 0; i < services.length; i++) {
     const service = await getServiceConfig(services[i].name);
-    serviceNames.push(service);
+    table.push([
+      service.SERVICE_NAME,
+      service.CIRCUIT_STATE,
+      process.env.SUBDOMAIN + service.SERVICE,
+    ]);
   }
 
-  console.log(serviceNames);
+  console.log(table.toString());
 };
 
 module.exports = list;
