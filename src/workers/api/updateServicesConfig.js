68f20163480c4ccd81d0f007946859aedb46f6c6
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-16 19:37:33 -0500

    Replaced forward slashes with %2F.

--- src/workers/api/updateServicesConfig.js
+++ src/workers/api/updateServicesConfig.js
@@ -2,54 +2,13 @@ const fetch = require("node-fetch");
 const configDir = require("../../utils/configDir");
 require("dotenv").config({ path: `${configDir}/.env` });
 
-async function getAccountId() {
-  const data = await fetch("https://api.cloudflare.com/client/v4/accounts", {
-    headers: {
-      "X-Auth-Email": process.env.EMAIL,
-      "X-Auth-Key": process.env.APIKEY,
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!data.ok) throw new Error("\nInvalid Credentials.");
-
-  const body = await data.json();
-
-  return body.result[0].id;
-}
-
-async function getNamespaceIds(accountId) {
-  const data = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces`,
-    {
-      method: "GET",
-      headers: {
-        "X-Auth-Email": process.env.EMAIL,
-        "X-Auth-Key": process.env.APIKEY,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  if (!data.ok) {
-    throw new Error(`\nUnable to retrieve KV Namespaces.`);
-  }
-
-  const body = await data.json();
-  return body.result;
-}
-
 async function updateServiceConfig(state) {
-  const accountId = await getAccountId();
-  const namespaces = await getNamespaceIds(accountId);
-  const namespaceId = namespaces.filter(
-    (kv) => kv.title === "SERVICES_CONFIG"
-  )[0].id;
-
-  const serviceId = state.SERVICE_NAME;
+  const accountId = process.env.ACCOUNT_ID;
+  const configId = process.env.SERVICES_CONFIG_ID;
+  const serviceId = state.SERVICE.replace(/\//g, '%2F');
 
   const data = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}/values/${serviceId}`,
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${configId}/values/${serviceId}/`,
     {
       method: "PUT",
       headers: {
