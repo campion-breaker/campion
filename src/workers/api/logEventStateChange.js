2018612cc4c0000b49ef0e2422394a9e5330b2f0
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-18 19:58:51 -0500

    Created log event state change that communicates to KV.

---
+++ src/workers/api/logEventStateChange.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+const configDir = require('../../utils/configDir');
+require('dotenv').config({ path: `${configDir}/.env` });
+
+const logEventStateChange = async (key) => {
+  const acctId = process.env.ACCOUNT_ID;
+  const eventsNamespaceId = process.env.EVENTS_ID;
+  const data = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${acctId}/storage/kv/namespaces/${eventsNamespaceId}/values/${key}`,
+    {
+      method: 'PUT',
+      headers: {
+        'X-Auth-Email': process.env.EMAIL,
+        'X-Auth-Key': process.env.APIKEY,
+        'Content-Type': 'text/plain',
+      },
+      body: '',
+    }
+  );
+};
+
+module.exports = logEventStateChange;
