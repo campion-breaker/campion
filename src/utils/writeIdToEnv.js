4fee886324dd98fe9ea3c131efde2bddb4098f29
Arthur Kauffman <arthur@sent.com>
2020-11-19 14:40:34 -0500

    Added functionality to write to React env at setup

--- src/utils/writeIdToEnv.js
+++ src/utils/writeIdToEnv.js
@@ -5,6 +5,7 @@ require("dotenv").config({ path: `${configDir}/.env` });
 const writeIdToEnv = (name, id) => {
   process.env[name] = id;
   fs.appendFileSync(`${configDir}/.env`, `\n${name}=${id}`);
+  fs.appendFileSync(`${__dirname}/../../front_end/app/.env`, `\nREACT_APP_${name}=${id}`);
 };
 
 module.exports = writeIdToEnv;
