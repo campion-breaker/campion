b23811e3e8fb0555f93d5923266bf78ddabddf4c
Arthur Kauffman <arthur@sent.com>
2020-11-20 18:46:14 -0500

    initial display of events

--- front_end/app/src/App.js
+++ front_end/app/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import os from 'os';
-import logo from './logo.svg';
-import './App.css';
-require('dotenv').config({ path: `${os.homedir()}/.campion/.env` });
+import React from "react";
+import logo from "./logo.svg";
+import moment from "moment";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -10,35 +9,53 @@ class App extends React.Component {
     traffic: [],
   };
 
-  componentDidMount() {
-    fetch('http://localhost:7777/events').then((data) => {
+  componentWillMount() {
+    fetch("http://localhost:7777/events").then((data) => {
       data.json().then((events, error) => {
         this.setState({ events });
       });
     });
 
-    fetch('http://localhost:7777/traffic').then((data) => {
+    fetch("http://localhost:7777/traffic").then((data) => {
       data.json().then((traffic, error) => {
-        console.log(traffic);
         this.setState({ traffic });
       });
     });
   }
 
   render() {
+    const events = this.state.events.slice(0, 10).map((event) => {
+      if (event.EVENT === "STATE_CHANGE") {
+        return (
+          <li key={event.TIME}>
+            <strong>
+              {event.ID} - {moment(event.TIME).fromNow()}
+            </strong>
+            : Circuit-breaker status changed from{" "}
+            {event.OLD_STATE.toLowerCase()} to {event.NEW_STATE.toLowerCase()}{" "}
+            {event.MODE === "AUTO" ? "automatically." : "by user."}
+          </li>
+        );
+      } else {
+        return (
+          <li key={event.TIME}>
+            {" "}
+            <strong>
+              {event.ID} - {moment(event.TIME).fromNow()}
+            </strong>
+            : Circuit-breaker settings were changed.
+          </li>
+        );
+      }
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Number of Events: {this.state.events.length}</p>
+          <ul>{events}</ul>
+          <p>Number of Traffic: {this.state.traffic.length}</p>
         </header>
       </div>
     );
