968261911ee43c41f551953850516cd26e93fafc
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-19 19:08:39 -0500

    Added the new dry version of getting KV info.

--- src/utils/getAllServicesConfigs.js
+++ src/utils/getAllServicesConfigs.js
@@ -1,8 +1,8 @@
 const getServiceConfig = require('../workers/api/getServiceConfig');
-const getAllServices = require('../workers/api/getAllServices');
+const getAllKeys = require('../workers/api/getAllKeys');
 
 const getAllServicesConfigs = async () => {
-  const services = await getAllServices();
+  const services = await getAllKeys('SERVICES_CONFIG_ID');
   let serviceNames = [];
 
   for (let i = 0; i < services.length; i++) {
