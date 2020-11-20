a88db0f8ab39e33643e230e80dd22efd35b1c22a
Ben Zelinski <bziggz@gmail.com>
2020-11-20 16:36:56 -0500

    Interact with individual config values

---
+++ src/workers/api/getServiceConfig.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+const configDir = require("../../utils/configDir");
+require("dotenv").config({ path: `${configDir}/.env` });
+
+async function getServiceConfig(id) {
+  const accountId = process.env.ACCOUNT_ID;
+  const configId = process.env.SERVICES_CONFIG_ID;
+  id = id.replace(/\//g, "%2F");
+
+  const data = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${configId}/values/${id}/`,
+    {
+      method: "GET",
+      headers: {
+        "X-Auth-Email": process.env.EMAIL,
+        "X-Auth-Key": process.env.APIKEY,
+        "Content-Type": "text/plain",
+      },
+    }
+  );
+
+  if (!data.ok) {
+    throw new Error(JSON.stringify(data)); //`\nFailed to update 'SERVICES_CONFIG'. Please try again.`);
+  }
+
+  const body = await data.json();
+  return body;
+}
+
+module.exports = getServiceConfig;
