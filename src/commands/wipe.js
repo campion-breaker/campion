c3c813ec75aea48597475b425c5f33994db0b1a9
Ben Zelinski <bziggz@gmail.com>
2020-11-20 17:04:54 -0500

    Campion wipe functionality added

---
+++ src/commands/wipe.js
@@ -0,0 +1,46 @@
+const prompt = require("prompts");
+const configDir = require("../utils/configDir");
+const { deleteWorker, deleteNamespace } = require("../workers/api/wipeCloudflare");
+const loadingBar = require("../utils/loadingBar");
+require("dotenv").config({ path: `${configDir}/.env` });
+
+const wipeSuccessMsg = () => {
+  console.log(
+    `\nAll Cloudflare information successfully deleted. Run 'campion setup' to redeploy.`
+  );
+};
+
+const confirm = () => {
+  return {
+    type: "confirm",
+    name: "value",
+    message: `Confirm deletion of Campion from Cloudflare.\n  This will erase all Circuit Breakers, settings, and protected endpoints.\n  This action cannot be undone.\n  Are you sure?`,
+    initial: false,
+  };
+};
+
+const wipe = async () => {
+  const confirmation = await prompt(confirm());
+
+  if (!confirmation.value) {
+    console.log("Campion deletion aborted.");
+    return;
+  }
+
+  const deleteServiceId = loadingBar(`\nDeleting Campion `);
+
+  try {
+    await deleteWorker();
+    await deleteNamespace(process.env.REQUEST_LOG_ID);
+    await deleteNamespace(process.env.SERVICES_CONFIG_ID);
+    await deleteNamespace(process.env.EVENTS_ID);
+    await deleteNamespace(process.env.TRAFFIC_ID);
+    clearInterval(deleteServiceId);
+    wipeSuccessMsg();
+  } catch (e) {
+    clearInterval(deleteServiceId);
+    console.log(e.message);
+  }
+};
+
+module.exports = wipe;
---
+++ src/workers/api/wipeCloudflare.js
@@ -0,0 +1,46 @@
+const fetch = require("node-fetch");
+const configDir = require("../../utils/configDir");
+require("dotenv").config({ path: `${configDir}/.env` });
+
+async function deleteWorker() {
+  const accountId = process.env.ACCOUNT_ID;
+
+  const deleteWorker = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/workers/scripts/campion/`,
+    {
+      method: "DELETE",
+      headers: {
+        "X-Auth-Email": process.env.EMAIL,
+        "X-Auth-Key": process.env.APIKEY,
+      },
+    }
+  );
+
+  if (!deleteWorker.ok) {
+    throw new Error(
+      `\nFailed to delete Campion from Cloudflare. Please try again.`
+    );
+  }
+}
+
+async function deleteNamespace(namespaceId) {
+  const accountId = process.env.ACCOUNT_ID;
+
+  const data = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}`,
+    {
+      method: "DELETE",
+      headers: {
+        "X-Auth-Email": process.env.EMAIL,
+        "X-Auth-Key": process.env.APIKEY,
+      },
+    }
+  );
+
+  if (!data.ok) {
+    throw new Error(`\nFailed to delete KV Namespace. Please try again.`);
+  }
+}
+
+exports.deleteWorker = deleteWorker;
+exports.deleteNamespace = deleteNamespace;
