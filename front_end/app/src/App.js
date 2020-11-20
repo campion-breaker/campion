d4b05630551271e0642fe82a534013e9f3e9b240
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-19 19:18:11 -0500

    Got the react app to connect to the back-end server and store KV state in the app state.

--- front_end/app/src/App.js
+++ front_end/app/src/App.js
@@ -1,26 +1,48 @@
-import os from "os";
+import React from 'react';
+import os from 'os';
 import logo from './logo.svg';
 import './App.css';
-require("dotenv").config({ path: `${os.homedir()}/.campion/.env` });
+require('dotenv').config({ path: `${os.homedir()}/.campion/.env` });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    events: [],
+    traffic: [],
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:7777/events').then((data) => {
+      data.json().then((events, error) => {
+        this.setState({ events });
+      });
+    });
+
+    fetch('http://localhost:7777/traffic').then((data) => {
+      data.json().then((traffic, error) => {
+        console.log(traffic);
+        this.setState({ traffic });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p></p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
