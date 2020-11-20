9265396a7e09c16168d4f9468c2aa8612930ba79
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-19 19:21:45 -0500

    Refactored the delete command to reflect the new way of interacting with the config state.

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -1,10 +1,10 @@
-const prompt = require("prompts");
-const getAllServicesConfigs = require("../utils/getAllServicesConfigs");
-const configDir = require("../utils/configDir");
-const configExists = require("../utils/validateConfig");
-const loadingBar = require("../utils/loadingBar");
-const deleteServiceRequest = require("../workers/api/deleteServiceRequest");
-require("dotenv").config({ path: `${configDir}/.env` });
+const prompt = require('prompts');
+const getAllKeys = require('../workers/api/getAllKeys');
+const configDir = require('../utils/configDir');
+const configExists = require('../utils/validateConfig');
+const loadingBar = require('../utils/loadingBar');
+const deleteServicesConfig = require('../workers/api/deleteServicesConfig');
+require('dotenv').config({ path: `${configDir}/.env` });
 
 const deleteServiceSuccessMsg = (service) => {
   console.log(`\nService '${service}' successfully deleted.`);
@@ -12,17 +12,17 @@ const deleteServiceSuccessMsg = (service) => {
 
 const questions = (choices) => {
   return {
-    type: "select",
-    name: "SERVICE",
-    message: "Which service would you like to delete? ",
+    type: 'select',
+    name: 'SERVICE',
+    message: 'Which service would you like to delete? ',
     choices,
   };
 };
 
 const confirm = (choice) => {
   return {
-    type: "confirm",
-    name: "value",
+    type: 'confirm',
+    name: 'value',
     message: `Confirm deletion of '${choice}'? This action cannot be reversed.`,
     initial: false,
   };
@@ -32,18 +32,13 @@ const selectService = async (services) => {
   const choices = services.map((service) => {
     return {
       title: service.SERVICE_NAME,
-      value: `${service.SERVICE}&&&${service.SERVICE_NAME}`,
+      value: service,
       description: service.SERVICE,
     };
   });
 
-  let chosenService = await prompt(questions(choices));
-  chosenService = chosenService.SERVICE.split("&&&");
-
-  return {
-    SERVICE: chosenService[0],
-    SERVICE_NAME: chosenService[1],
-  };
+  const service = await prompt(questions(choices));
+  return service.SERVICE;
 };
 
 const deleteService = async () => {
@@ -53,19 +48,19 @@ const deleteService = async () => {
   }
 
   let services;
-  const retrieveId = loadingBar("Retrieving services ");
+  const retrieveId = loadingBar('Retrieving services ');
 
   try {
-    services = await getAllServicesConfigs();
+    services = await getAllKeys('SERVICES_CONFIG_ID');
     clearInterval(retrieveId);
-    console.log("\n");
+    console.log('\n');
   } catch (e) {
     clearInterval(retrieveId);
     console.log(e.message);
   }
 
   if (services.length === 0) {
-    console.log("\nNo services found.");
+    console.log('\nNo services found.');
     return;
   }
 
@@ -73,7 +68,7 @@ const deleteService = async () => {
   const confirmation = await prompt(confirm(chosenService.SERVICE_NAME));
 
   if (!confirmation.value) {
-    console.log("Delete aborted.");
+    console.log('Delete aborted.');
     return;
   }
 
@@ -82,7 +77,7 @@ const deleteService = async () => {
   );
 
   try {
-    await deleteServiceRequest(chosenService.SERVICE);
+    await deleteServicesConfig(chosenService);
     clearInterval(deleteServiceId);
     deleteServiceSuccessMsg(chosenService.SERVICE_NAME);
   } catch (e) {
