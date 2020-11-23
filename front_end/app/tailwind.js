e24a5c142abe959422e474e5a85a2299b1e789e1
Arthur Kauffman <arthur@sent.com>
2020-11-23 17:40:17 -0500

    update tailwind config to purge

--- front_end/app/tailwind.js
+++ front_end/app/tailwind.js
@@ -1,7 +1,9 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.jsx'
+  ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
