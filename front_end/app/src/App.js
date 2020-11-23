6d0c71e91cc62e2f1116d1decdeb231407c62a9f
Arthur Kauffman <arthur@sent.com>
2020-11-23 18:45:11 -0500

    Add endpoints component

--- front_end/app/src/App.js
+++ front_end/app/src/App.js
@@ -1,12 +1,12 @@
 import EventsList from "./components/EventsList";
 import TrafficList from "./components/TrafficList";
 import Sidebar from "./components/Sidebar";
+import EndpointsList from "./components/EndpointsList";
 
 const App = () => {
   return (
     <div className="App">
       <div className="h-screen flex overflow-hidden bg-gray-100">
-
         <Sidebar />
 
         <div className="flex flex-col w-0 flex-1 overflow-hidden">
@@ -36,7 +36,9 @@ const App = () => {
           >
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Performance Snapshot</h1>
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  Performance Snapshot
+                </h1>
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 <div className="py-4">
@@ -46,7 +48,21 @@ const App = () => {
             </div>
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Activity Log</h1>
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  Protected Endpoints
+                </h1>
+              </div>
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <div className="py-4">
+                  <EndpointsList />
+                </div>
+              </div>
+            </div>
+            <div className="py-6">
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  Activity Log
+                </h1>
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 <div className="py-4">
