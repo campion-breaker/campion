6186641bb116501e0d21c15d2ea00820d1e58bdb
Arthur Kauffman <arthur@sent.com>
2020-11-23 18:14:13 -0500

    install postcss and configure purging

--- front_end/app/postcss.config.js
+++ front_end/app/postcss.config.js
@@ -1,9 +1,7 @@
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.js'),
     require('postcss-import'),
+    require('tailwindcss'),
     require('autoprefixer'),
   ]
 }
