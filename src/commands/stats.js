2350df274e259a81639f05a18b461592387497f9
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-25 22:46:19 -0500

    Made it so that in order to run 'campion stats' the config file must exist.

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,7 +1,13 @@
 const { spawn } = require('child_process');
 const open = require('open');
+const configExists = require('../utils/validateConfig');
 
 const stats = () => {
+  if (!configExists()) {
+    console.log('Config not found. Run "campion setup" to start.');
+    return;
+  }
+
   const server = spawn('start_server');
   server.stdout.on('data', (data) => {
     console.log(`${data}`);
