37ac0fc16ee00c62e6ee47b8293e7bcd95c8807b
Ben Zelinski <bziggz@gmail.com>
2020-11-30 15:49:15 -0500

    Refactoring File Tree for aws v cf

--- src/utils/configDir.js
+++
@@ -1,6 +0,0 @@
-const os = require("os");
-const homedir = os.homedir();
-const configDir = ".campion";
-
-module.exports = `${homedir}/${configDir}`;
-
--- src/utils/loadingBar.js
+++
@@ -1,8 +0,0 @@
-const startLoadingBar = (message) => {
-  process.stdout.write(`\n${message}`);
-  return setInterval(() => {
-    process.stdout.write(".");
-  }, 300);
-};
-
-module.exports = startLoadingBar;
