82e6dc7359f4fdd7b40061da791f10a36f5b739b
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-30 21:34:19 -0500

    Refactored code.

--- src/aws/api/lambda/createFunction.js
+++ src/aws/api/lambda/createFunction.js
@@ -1,21 +1,19 @@
-const { lambda } = require("../sdk");
-const fs = require("fs");
+const { lambda } = require('../sdk');
+const fs = require('fs');
 
 const createFunction = (name) => {
-  const code = fs.readFileSync(
-    `${__dirname}/../../circuitBreaker.zip`
-  );
+  const code = fs.readFileSync(`${__dirname}/../../circuitBreaker.zip`);
 
   const params = {
-    Description: "Campion Circuit-Breaker",
+    Description: 'Campion Circuit-Breaker',
     FunctionName: name,
     Publish: true,
-    Handler: "index.handler",
-    Runtime: "nodejs12.x",
-    Role: process.env.ROLE_ARN,
+    Handler: 'index.handler',
+    Runtime: 'nodejs12.x',
+    Role: process.env.AWS_ROLE_ARN,
     Code: {
-      ZipFile: code
-    }
+      ZipFile: code,
+    },
   };
 
   return lambda.createFunction(params).promise();
