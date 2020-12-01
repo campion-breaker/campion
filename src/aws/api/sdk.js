227a33dbe8d92e777275b4a0f954ed1bc3f3f6f2
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-11-30 19:43:40 -0500

    Added the CloudFront object and exported it.

--- src/aws/api/sdk.js
+++ src/aws/api/sdk.js
@@ -1,21 +1,21 @@
-const AWS = require("aws-sdk");
-const configDir = require("../utils/configDir");
-require("dotenv").config({ path: `${configDir}/.env` });
+const AWS = require('aws-sdk');
+const configDir = require('../utils/configDir');
+require('dotenv').config({ path: `${configDir}/.env` });
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY, 
-  region: "us-east-1",
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: 'us-east-1',
 });
 
 const iam = new AWS.IAM();
 const ddb = new AWS.DynamoDB();
 const lambda = new AWS.Lambda();
+const cloudFront = new AWS.CloudFront();
 
 module.exports = {
-  iam, 
+  iam,
   ddb,
   lambda,
+  cloudFront,
 };
-
-
