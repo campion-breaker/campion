7dc5011b0e43f07360da5b3f5bb08bd0a9a2ce02
Arthur Kauffman <arthur@sent.com>
2020-12-01 13:24:49 -0500

    updated circuitbreaker zip

--- src/aws/index.js
+++ src/aws/index.js
@@ -1,22 +1,22 @@
-const AWS = require("aws-sdk");
-const crypto = require("crypto");
-const generateUUID = () => crypto.randomBytes(16).toString("hex");
+const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient();
 const https = require('https');
 const url = require('url');
 
-async function flipState(serviceName, newState) {
+async function flipState(service, newState) {
+  await logEventStateChange(service, newState);
+
   const params = {
-    TableName: "RESOURCES",
+    TableName: 'SERVICES_CONFIG',
     Key: {
-      ServiceName: serviceName,
+      ID: service.ID,
     },
     UpdateExpression: `SET CIRCUIT_STATE=:s, UPDATED_TIME=:t`,
     ExpressionAttributeValues: {
-      ":s": newState,
-      ":t": Date.now(),
+      ':s': newState,
+      ':t': Date.now(),
     },
-    ReturnValues: "UPDATED_NEW",
+    ReturnValues: 'UPDATED_NEW',
   };
 
   try {
@@ -25,10 +25,7 @@ async function flipState(serviceName, newState) {
         console.log(data);
       })
       .promise();
-    const response = {
-      statusCode: 200,
-    };
-    return response;
+    return { statusCode: 200 };
   } catch (e) {
     return {
       statusCode: 500,
@@ -37,12 +34,11 @@ async function flipState(serviceName, newState) {
   }
 }
 
-async function logFailure(failureKey) {
+async function putDB(tableName, items) {
   const params = {
-    TableName: "FAILURES",
+    TableName: tableName,
     Item: {
-      ServiceName: failureKey,
-      Time: Date.now() + 60000,
+      ...items,
     },
   };
 
@@ -64,8 +60,8 @@ async function dbConfigRead(tableName, serviceName) {
   const params = {
     TableName: tableName,
     Key: {
-      'ServiceName': serviceName
-    }
+      ID: serviceName,
+    },
   };
 
   try {
@@ -79,16 +75,14 @@ async function dbConfigRead(tableName, serviceName) {
   }
 }
 
-async function dbFailureRead(tableName, serviceName) {
+async function dbFailureRead(tableName) {
   const params = {
     TableName: tableName,
   };
 
   try {
     const data = await documentClient.scan(params).promise();
-    const response = {
-      body: data.Items,
-    };
+    const response = data.Items;
     return response;
   } catch (e) {
     return {
@@ -97,117 +91,102 @@ async function dbFailureRead(tableName, serviceName) {
   }
 }
 
+const newResponse = (body, status, headers) => {
+  return {
+    body,
+    headers: Object.assign({}, headers, status),
+  };
+};
+
 async function handleRequest(request) {
-  // parse url for serviceName
-  const serviceName = "SuccessService";
-  const config = await dbConfigRead('RESOURCES', serviceName);
-  const circuitStateObj = await circuitState(serviceName, config, request);
+  const serviceId = 'https://arthurkauffman.com';
+  const service = await dbConfigRead('SERVICES_CONFIG', serviceId);
+  const receivedTime = Date.now();
 
-  if (circuitStateObj.state === "OPEN") {
-    return {body: {failureKind: circuitStateObj.failureKind}, headers: {status: 504}};
-  } else if (circuitStateObj.state === "HALF-OPEN" && !canRequestProceed(config)) {
-    return {body: {failureKind: circuitStateObj.failureKind}, headers: {status: 504}};
+  if (service === null) {
+    return newResponse("Circuit breaker doesn't exist", 404);
   }
 
-  const response = await processRequest(request, config, serviceName);
-  if (response.failure) {
-    logFailure(response.key);
-  } else if (!response.failure && circuitStateObj.state === "HALF-OPEN") {
-    logFailure(response.key);
+  if (service.CIRCUIT_STATE === 'FORCED-OPEN') {
+    await logRequestMetrics(receivedTime, service);
+    return newResponse(
+      'Circuit has been manually force-opened. Adjust in Campion CLI/GUI.',
+      504
+    );
   }
 
-  return {body: response.body, status: response.status, headers: response.headers};
+  if (service.CIRCUIT_STATE === 'OPEN') {
+    await setStateWhenOpen(service, serviceId);
+    if (service.CIRCUIT_STATE === 'OPEN') {
+      await logRequestMetrics(receivedTime, service);
+      return newResponse('Circuit is open', 504);
+    }
+  }
+
+  if (service.CIRCUIT_STATE === 'HALF-OPEN' && !canRequestProceed(service)) {
+    await logRequestMetrics(receivedTime, service);
+    return newResponse('Circuit is half-open', 504);
+  }
+
+  const response = await processRequest(service);
+  console.log('response', response);
+  const responseTime = Date.now();
+
+  await updateCircuitState(service, serviceId, response);
+  await logRequestMetrics(receivedTime, service, responseTime, response.status);
+
+  return newResponse(response.body, response.status, response.headers);
 }
 
-function canRequestProceed(config) {
+function canRequestProceed(service) {
   const min = 1;
-  const max = Math.floor(100 / config.PERCENT_OF_REQUESTS);
+  const max = Math.floor(100 / service.PERCENT_OF_REQUESTS);
   const randNum = Math.floor(Math.random() * (max - min + 1) + min);
   return randNum === 1;
 }
 
-async function circuitState(serviceName, config, request) {
-  const { serviceFailures, networkFailures, successes } = await failureCount(
-    request, serviceName
-  );
-  let failureKind;
-  const state = config.CIRCUIT_STATE;
-  const response = { state };
-
-  if (
-    state === "CLOSED" && (serviceFailures >= config.SERVICE_FAILURE_THRESHOLD ||
-    networkFailures >= config.NETWORK_FAILURE_THRESHOLD)
-  ) {
-    failureKind =
-      serviceFailures >= config.SERVICE_FAILURE_THRESHOLD
-        ? "Service failure"
-        : "Network failure";
-    flipState(serviceName, "OPEN");
-  } else if (state === "OPEN") {
-    const now = Date.now();
-    const oldDate = Number(config.UPDATED_TIME);
-    const differenceInSecs = (now - oldDate) / 1000;
-    if (differenceInSecs >= config.ERROR_TIMEOUT) {
-      flipState(serviceName, "HALF-OPEN");
-      response.state = "HALF-OPEN";
-    }
-    response.failureKind = failureKind;
-  } else if (state === "HALF-OPEN") {
-    if (successes >= 1) {
-      flipState(serviceName, "CLOSED");
-    } else if (
-      networkFailures >= config.NETWORK_FAILURE_THRESHOLD ||
-      serviceFailures >= config.SERVICE_FAILURE_THRESHOLD
-    ) {
-      flipState(serviceName, "OPEN");
-    }
-  }
-
-  return response;
-}
-
-async function failureCount(request, serviceName) {
-  const list = await dbFailureRead('FAILURES');
-  const failures = list.body.filter((obj) => obj.ServiceName.includes(serviceName));
-  const serviceFailures = failures.filter((obj) =>
-    obj.ServiceName.includes("@SERVICE_FAILURE_")
-  ).length;
-  const networkFailures = failures.filter((obj) =>
-    obj.ServiceName.includes("@NETWORK_FAILURE_")
-  ).length;
-  const successes = failures.filter((obj) => obj.ServiceName.includes("@SUCCESS_"))
-    .length;
-  return { serviceFailures, networkFailures, successes };
-}
-
-async function processRequest(request, config, serviceName) {
+async function processRequest(service) {
   let timeoutId;
-  const timeoutPromise = new Promise((resolutionFunc, rejectionFunc) => {
+
+  const timeoutPromise = new Promise((resolutionFunc) => {
     timeoutId = setTimeout(() => {
-      resolutionFunc({ failure: true, kvKey: "@NETWORK_FAILURE_" + serviceName + generateUUID(), status: 522 });
-    }, config.MAX_LATENCY);
+      resolutionFunc({
+        failure: true,
+        kvKey: '@NETWORK_FAILURE_' + Date.now(),
+        status: 522,
+      });
+    }, service.MAX_LATENCY);
   });
 
   const fetchPromise = new Promise((resolve, reject) => {
-    const req = https.request(Object.assign({}, url.parse(config.SERVICE), {method: 'GET'}), res => {
-      let body, failure, key;
-      res.on('data', (data) => {
-        clearTimeout(timeoutId);
-        failure = false;
-        key = "@SUCCESS_" + serviceName + generateUUID();
-        body = data.toString();
-      })
+    const req = https.request(
+      Object.assign({}, url.parse(service.ID), { method: 'GET' }),
+      (res) => {
+        let body, failure, key;
+        res.on('data', (data) => {
+          clearTimeout(timeoutId);
+          failure = false;
+          key = '@SUCCESS_' + service.ID + Date.now();
+          body = data.toString();
+        });
 
-      res.on('error', reject);
-      res.on('end', () => {
-        if (res.statusCode >= 500) {
-          failure = true;
-          key = "@SERVICE_FAILURE_" + serviceName + generateUUID();
-        }
+        res.on('error', reject);
+        res.on('end', () => {
+          if (res.statusCode >= 500) {
+            failure = true;
+            key = '@SERVICE_FAILURE_' + service.ID + Date.now();
+          }
 
-        resolve({status: res.statusCode, headers: res.headers, body: body, failure, key});
-      });
-    });
+          resolve({
+            status: res.statusCode,
+            headers: res.headers,
+            body: body,
+            failure,
+            key,
+          });
+        });
+      }
+    );
 
     req.on('error', (e) => {
       console.error(e);
@@ -220,28 +199,130 @@ async function processRequest(request, config, serviceName) {
     return value;
   });
 }
+
+async function setStateWhenClosed(service, serviceId) {
+  const { serviceFailures, networkFailures } = await requestLogCount(serviceId);
+
+  if (
+    serviceFailures >= service.SERVICE_FAILURE_THRESHOLD ||
+    networkFailures >= service.NETWORK_FAILURE_THRESHOLD
+  ) {
+    await flipState(service, 'OPEN');
+  }
+}
+
+async function setStateWhenOpen(service, serviceId) {
+  const now = Date.now();
+  const oldDate = service.UPDATED_TIME;
+  const differenceInSecs = (now - oldDate) / 1000;
+
+  if (differenceInSecs >= service.ERROR_TIMEOUT) {
+    await flipState(service, 'HALF-OPEN');
+  }
+}
+
+async function setStateWhenHalfOpen(service, serviceId) {
+  const { successes, serviceFailures, networkFailures } = await requestLogCount(
+    serviceId
+  );
+
+  if (successes >= service.SUCCESS_THRESHOLD) {
+    await flipState(service, 'CLOSED');
+  } else if (
+    networkFailures >= service.NETWORK_FAILURE_THRESHOLD ||
+    serviceFailures >= service.SERVICE_FAILURE_THRESHOLD
+  ) {
+    await flipState(service, 'OPEN');
+  }
+}
+
+async function updateCircuitState(service, serviceId, response) {
+  if (
+    response.failure ||
+    (service.CIRCUIT_STATE === 'HALF-OPEN' && !response.failure)
+  ) {
+    await putDB('REQUEST_LOG', { ID: response.key });
+  }
+
+  switch (service.CIRCUIT_STATE) {
+    case 'CLOSED':
+      await setStateWhenClosed(service, serviceId);
+      break;
+    case 'HALF-OPEN':
+      await setStateWhenHalfOpen(service, serviceId);
+      break;
+  }
+}
+
+async function logEventStateChange(service, newState) {
+  const stateChangeEntry = {
+    ID: service.ID,
+    NAME: service.NAME,
+    EVENT: 'STATE_CHANGE',
+    TIME: Date.now(),
+    OLD_STATE: service.CIRCUIT_STATE,
+    CIRCUIT_STATE: newState,
+    MODE: 'AUTO',
+  };
+
+  await putDB('EVENTS', stateChangeEntry);
+}
+
+async function requestLogCount(serviceId) {
+  const list = await dbFailureRead('REQUEST_LOG');
+  const log = list.filter((obj) => obj.ID.includes(serviceId));
+  const serviceFailures = log.filter((obj) =>
+    obj.ID.includes('@SERVICE_FAILURE_')
+  ).length;
+  const networkFailures = log.filter((obj) =>
+    obj.ID.includes('@NETWORK_FAILURE_')
+  ).length;
+  const successes = log.filter((obj) => obj.ID.includes('@SUCCESS_')).length;
+  return { serviceFailures, networkFailures, successes };
+}
+
+async function logRequestMetrics(receivedTime, service, responseTime, status) {
+  const key = JSON.stringify({
+    ID: service.ID,
+    STATUS: status || '',
+    STATE: service.CIRCUIT_STATE,
+    TIME: receivedTime,
+    LATENCY: responseTime - receivedTime || '',
+  });
+
+  await putDB('TRAFFIC', key);
+}
+
 const blacklistedHeaders = [
-  'connection', 'expect', 'keep-alive', 'proxy-authenticate', 
-  'proxy-authorization', 'proxy-connection', 'trailer', 'upgrade',
-  'x-accel-buffering', 'x-accel-charset', 'x-accel-limit-rate', 
-  'x-accel-redirect', 'x-cache', 'x-edge', 
-  'x-forward-proto', 'x-real-ip'
-  ];
-  
-const readOnlyHeaders = [
-  'content-length', 'host', 'transfer-encoding', 'via'
-  ];
-  
+  'connection',
+  'expect',
+  'keep-alive',
+  'proxy-authenticate',
+  'proxy-authorization',
+  'proxy-connection',
+  'trailer',
+  'upgrade',
+  'x-accel-buffering',
+  'x-accel-charset',
+  'x-accel-limit-rate',
+  'x-accel-redirect',
+  'x-cache',
+  'x-edge',
+  'x-forward-proto',
+  'x-real-ip',
+];
+
+const readOnlyHeaders = ['content-length', 'host', 'transfer-encoding', 'via'];
+
 exports.handler = async (event, context, callback) => {
   const response = await handleRequest(event.request);
 
   Object.keys(response.headers).forEach((key) => {
-    response.headers[key] = [{ key: key, value: response.headers[key]}]
+    response.headers[key] = [{ key: key, value: response.headers[key] }];
     if (blacklistedHeaders.includes(key) || readOnlyHeaders.includes(key)) {
       delete response.headers[key];
     }
   });
-  
+
   return callback(null, response);
 };
-
