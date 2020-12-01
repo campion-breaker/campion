0e8ac835e08207d18e5a786d9d2b2ca8cff39d89
Ben Zelinski <bziggz@gmail.com>
2020-12-01 14:34:22 -0500

    Create dynamoDB put method

---
+++ src/aws/api/dynamoDB/putToTable.js
@@ -0,0 +1,28 @@
+const { ddb } = require("../sdk");
+
+async function putToTable(tableName, item) {
+  const params = {
+    TableName: tableName,
+    Item: formatItem(item),
+  };
+  console.log(params);
+  return ddb.putItem(params).promise();
+}
+
+const formatItem = (item) => {
+  const attrType = (attr) => {
+    return typeof attr === "string" ? "S" : "N";
+  };
+
+  const result = {};
+
+  Object.keys(item).forEach((attr) => {
+    result[attr] = {
+      [attrType(item[attr])]: item[attr].toString(),
+    };
+  });
+
+  return result;
+};
+
+module.exports = putToTable;
