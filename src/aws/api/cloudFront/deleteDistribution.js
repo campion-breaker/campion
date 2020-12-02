09612496ddc972e0b7a41b49f28718a4b604f990
Ben Zelinski <bziggz@gmail.com>
2020-12-02 16:31:47 -0500

    Successful Cloudfront distribution deletion

--- src/aws/api/cloudFront/deleteDistribution.js
+++ src/aws/api/cloudFront/deleteDistribution.js
@@ -1,11 +1,45 @@
-const { cloudFront } = require('../sdk');
+const { cloudFront } = require("../sdk");
 
-const deleteDistribution = (Id) => {
-  const params = {
+const deleteDistribution = async (Id) => {
+  let e_tag;
+  let disabled;
+  async function disableCloudfrontDistribution() {
+    const previousDistributionConfig = await cloudFront
+      .getDistributionConfig({
+        Id,
+      })
+      .promise();
+    e_tag = previousDistributionConfig.ETag;
+
+    previousDistributionConfig.DistributionConfig.Enabled = false;
+
+    const disableParams = {
+      Id,
+      DistributionConfig: previousDistributionConfig.DistributionConfig,
+      IfMatch: e_tag,
+    };
+    return cloudFront.updateDistribution(disableParams).promise();
+  }
+
+  await disableCloudfrontDistribution(Id);
+
+  while (disabled !== "Deployed") {
+    const previousDistribution = await cloudFront
+      .getDistribution({
+        Id,
+      })
+      .promise();
+
+    disabled = previousDistribution.Distribution.Status;
+    e_tag = previousDistribution.ETag;
+  }
+
+  const deleteParams = {
     Id,
+    IfMatch: e_tag,
   };
 
-  return cloudFront.deleteDistribution(params).promise();
+  return cloudFront.deleteDistribution(deleteParams).promise();
 };
 
 module.exports = deleteDistribution;
