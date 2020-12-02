890da1ef87aed238554a9d00ad7f840ae277066c
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-12-02 13:49:45 -0500

    Uncommented the wipe command.

--- bin/campionaws.js
+++ bin/campionaws.js
@@ -1,54 +1,54 @@
 #!/usr/bin/env node
 
-const { program } = require("commander");
+const { program } = require('commander');
 
-program.version("0.0.1");
-
-program
-  .command("s")
-  .alias("setup")
-  .description("Configure Campion with AWS Email and IAM API Key")
-  .action(() => require("../src/aws/commands/setup")());
+program.version('0.0.1');
 
 program
-  .command("a")
-  .alias("add")
-  .description("Protect a new endpoint")
-  .action(() => require("../src/aws/commands/add")());
+  .command('s')
+  .alias('setup')
+  .description('Configure Campion with AWS Email and IAM API Key')
+  .action(() => require('../src/aws/commands/setup')());
 
 program
-  .command("l")
-  .alias("list")
-  .description("List all protected services")
-  .action(() => require("../src/aws/commands/list")());
+  .command('a')
+  .alias('add')
+  .description('Protect a new endpoint')
+  .action(() => require('../src/aws/commands/add')());
 
 program
-  .command("u")
-  .alias("update")
-  .description("Update the config parameters for a single endpoint")
-  .action(() => require("../src/aws/commands/update")());
+  .command('l')
+  .alias('list')
+  .description('List all protected services')
+  .action(() => require('../src/aws/commands/list')());
 
 program
-  .command("d")
-  .alias("delete")
-  .description("Delete a service from Campion")
-  .action(() => require("../src/aws/commands/delete")());
+  .command('u')
+  .alias('update')
+  .description('Update the config parameters for a single endpoint')
+  .action(() => require('../src/aws/commands/update')());
 
 program
-  .command("f")
-  .alias("flip")
+  .command('d')
+  .alias('delete')
+  .description('Delete a service from Campion')
+  .action(() => require('../src/aws/commands/delete')());
+
+program
+  .command('f')
+  .alias('flip')
   .description("Manually flip the state of a service's circuit breaker")
-  .action(() => require("../src/aws/commands/flip")());
+  .action(() => require('../src/aws/commands/flip')());
 
 // program
 //   .command("stats")
 //   .description("View Campion stats in a browser window.")
 //   .action(() => require("../src/aws/commands/stats")());
 
-// program
-//   .command("w")
-//   .alias("wipe")
-//   .description("Clear all Campion information from AWS.")
-//   .action(() => require("../src/aws/commands/wipe")());
+program
+  .command('w')
+  .alias('wipe')
+  .description('Clear all Campion information from AWS.')
+  .action(() => require('../src/aws/commands/wipe')());
 
 program.parse(process.argv);
