2a1bb7d6fb8fb709fb9f948df4a2e42a433d67ed
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-12-02 13:50:23 -0500

    Requiring the dotenv.

--- src/aws/api/iam/deleteRolePolicy.js
+++ src/aws/api/iam/deleteRolePolicy.js
@@ -1,5 +1,5 @@
 const { iam } = require('../sdk');
-const configDir = require('../utils/configDir');
+const configDir = require('../../utils/configDir');
 require('dotenv').config({ path: `${configDir}/.env` });
 
 const deleteRolePolicy = (PolicyName) => {
@@ -8,7 +8,7 @@ const deleteRolePolicy = (PolicyName) => {
     RoleName: process.env.AWS_ROLE_NAME,
   };
 
-  return iam.deleteRolePolicy.promise();
+  return iam.deleteRolePolicy(params).promise();
 };
 
 module.exports = deleteRolePolicy;
