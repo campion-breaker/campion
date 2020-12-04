29b11d8c6619d0e466b3b9c1d4b5f3d1e894a049
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-12-03 21:05:21 -0500

    Fixed the subdomain route to return an absolute subdomain.

--- front_end/server/awsserver.js
+++ front_end/server/awsserver.js
@@ -1,48 +1,48 @@
 #!/usr/bin/env node
 
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const getFromTable = require(`${__dirname}/../../src/aws/api/dynamoDB/getFromTable`);
-const configDir = require("../../src/aws/utils/configDir");
-require("dotenv").config({ path: `${configDir}/.env` });
+const configDir = require('../../src/aws/utils/configDir');
+require('dotenv').config({ path: `${configDir}/.env` });
 const app = express();
-const host = "localhost";
+const host = 'localhost';
 const port = 7777;
 
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname + "/../app/build/")));
+app.use(express.static(path.resolve(__dirname + '/../app/build/')));
 
-app.get("/", (_, res) => {
-  res.sendFile(path.resolve(__dirname + "/../app/build/index.html"));
+app.get('/', (_, res) => {
+  res.sendFile(path.resolve(__dirname + '/../app/build/index.html'));
 });
 
-app.get("/events", async (req, res) => {
-  const events = await getFromTable("EVENTS");
-  events.forEach((event) => (event.ID = event.ID.split("_")[0]));
-  res.set("Content-Type", "application/json");
+app.get('/events', async (req, res) => {
+  const events = await getFromTable('EVENTS');
+  events.forEach((event) => (event.ID = event.ID.split('_')[0]));
+  res.set('Content-Type', 'application/json');
   res.send(events);
 });
 
-app.get("/traffic", async (req, res) => {
-  const traffic = await getFromTable("TRAFFIC");
-  res.set("Content-Type", "application/json");
+app.get('/traffic', async (req, res) => {
+  const traffic = await getFromTable('TRAFFIC');
+  res.set('Content-Type', 'application/json');
   res.send(traffic);
 });
 
-app.get("/endpoints", async (req, res) => {
-  const endpoints = await getFromTable("SERVICES_CONFIG");
-  res.set("Content-Type", "application/json");
+app.get('/endpoints', async (req, res) => {
+  const endpoints = await getFromTable('SERVICES_CONFIG');
+  res.set('Content-Type', 'application/json');
   res.send(endpoints);
 });
 
-app.get("/subdomain", (req, res) => {
-  res.set("Content-Type", "application/json");
-  res.send(JSON.stringify(process.env.AWS_DOMAIN_NAME));
+app.get('/subdomain', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(JSON.stringify('https://' + process.env.AWS_DOMAIN_NAME));
 });
 
 app.listen(port, host, () => {
   console.log(`\nCampion stats are now available at http://${host}:${port}.`);
-  console.log("Control + C to quit.");
+  console.log('Control + C to quit.');
 });
