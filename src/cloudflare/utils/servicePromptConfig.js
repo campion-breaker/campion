5860b9370fb16710663c2d2b819a72f6aafe5f88
Benjamin Zelinski <benjaminzelinski@benjamins-mbp.home>
2020-12-11 15:23:59 -0500

    Fixed typo

--- src/cloudflare/utils/servicePromptConfig.js
+++ src/cloudflare/utils/servicePromptConfig.js
@@ -1,21 +1,21 @@
-const prompt = require('prompts');
-const fetch = require('node-fetch');
+const prompt = require("prompts");
+const fetch = require("node-fetch");
 
 const initialState = {
-  CIRCUIT_STATE: 'CLOSED',
+  CIRCUIT_STATE: "CLOSED",
   ERROR_TIMEOUT: 60,
   MAX_LATENCY: 3000,
   NETWORK_FAILURE_THRESHOLD: 5,
   PERCENT_OF_REQUESTS: 25,
-  ID: '',
-  NAME: '',
+  ID: "",
+  NAME: "",
   SERVICE_FAILURE_THRESHOLD: 3,
   SUCCESS_THRESHOLD: 2,
   TIMESPAN: 60,
 };
 
 const formatUrl = (url) => {
-  if (url.slice(-1) === '/') {
+  if (url.slice(-1) === "/") {
     return url.slice(0, -1);
   }
 
@@ -24,65 +24,65 @@ const formatUrl = (url) => {
 
 const questions = (state) => [
   {
-    type: 'text',
-    name: 'ID',
-    message: 'Enter the service URL: ',
-    initial: state.ID || '',
+    type: "text",
+    name: "ID",
+    message: "Enter the service URL: ",
+    initial: state.ID || "",
     format: (url) => formatUrl(url),
   },
   {
-    type: 'text',
-    name: 'NAME',
-    message: 'Enter the name of the service: ',
-    initial: state.NAME || '',
+    type: "text",
+    name: "NAME",
+    message: "Enter the name of the service: ",
+    initial: state.NAME || "",
   },
   {
-    type: 'number',
-    name: 'MAX_LATENCY',
+    type: "number",
+    name: "MAX_LATENCY",
     message:
       "How many milliseconds should Campion wait until the request is considered 'failed'?",
     initial: state.MAX_LATENCY || 1000,
   },
   {
-    type: 'number',
-    name: 'TIMESPAN',
+    type: "number",
+    name: "TIMESPAN",
     message:
-      'How far back (sec) should Campion look for failures (aka TIMESPAN)?',
+      "How far back (sec) should Campion look for failures (aka TIMESPAN)?",
     initial: state.TIMESPAN || 60,
   },
   {
-    type: 'number',
-    name: 'NETWORK_FAILURE_THRESHOLD',
+    type: "number",
+    name: "NETWORK_FAILURE_THRESHOLD",
     message:
-      'How many NETWORK FAILURES (within TIMESPAN) will open the Breaker?',
+      "How many NETWORK FAILURES (within TIMESPAN) will open the Breaker?",
     initial: state.NETWORK_FAILURE_THRESHOLD || 5,
   },
   {
-    type: 'number',
-    name: 'SERVICE_FAILURE_THRESHOLD',
+    type: "number",
+    name: "SERVICE_FAILURE_THRESHOLD",
     message:
-      'How many SERVICE FAILURES (within TIMESPAN) will open the Breaker?',
+      "How many SERVICE FAILURES (within TIMESPAN) will open the Breaker?",
     initial: state.SERVICE_FAILURE_THRESHOLD || 3,
   },
   {
-    type: 'number',
-    name: 'ERROR_TIMEOUT',
+    type: "number",
+    name: "ERROR_TIMEOUT",
     message:
-      'After opening, how long (sec) should Campion wait until it begins sending test traffic?',
+      "After opening, how long (sec) should Campion wait until it begins sending test traffic?",
     initial: state.ERROR_TIMEOUT || 60,
   },
   {
-    type: 'number',
-    name: 'PERCENT_OF_REQUESTS',
+    type: "number",
+    name: "PERCENT_OF_REQUESTS",
     message:
-      'When Campion is HALF_OPEN, what percentage of requests should it send as tests?',
+      "When Campion is HALF-OPEN, what percentage of requests should it send as tests?",
     initial: state.PERCENT_OF_REQUESTS || 25,
   },
   {
-    type: 'number',
-    name: 'SUCCESS_THRESHOLD',
+    type: "number",
+    name: "SUCCESS_THRESHOLD",
     message:
-      'When Campion is HALF-OPEN, how many successes (within TIMESPAN) will close the Breaker?',
+      "When Campion is HALF-OPEN, how many successes (within TIMESPAN) will close the Breaker?",
     initial: state.SUCCESS_THRESHOLD || 2,
   },
 ];
